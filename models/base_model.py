41c9aac46077e01bb6cff61138d1edec9f981e82
Ernesto Boca <boca.ernesto13@gmail.com>
2024-02-07 18:03:34 -0500

    Missing tests for the BaseModel class

---
+++ models/base_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""This module contains the BaseModel Class, that defines all
+common attributes/methods for other classes"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Defines all common attributes/methods for other classes"""
+
+    def __init__(self):
+        """Initializes the object"""
+        self.id = uuid.uuid4()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Prints the BaseModel object"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                self.id, self.__dict__)
+
+    def save(self):
+        """updates the public instance updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values
+        of __dict__ of the instance"""
+        my_dict = {}
+        for key, value in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                my_dict[key] = value.strftime("%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                my_dict[key] = value
+
+        my_dict["__class__"] = self.__class__.__name__
+
+        return my_dict
+        
